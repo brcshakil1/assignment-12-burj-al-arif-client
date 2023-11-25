edef0651ce2ccf56d87fb4d93286b7c8a69bf73c
brcshakil1 <brcshakil72@gmail.com>
2023-11-26 01:58:44 +0600

    footer section's styling done

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,5 +1,34 @@
+import { Container } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <Container maxWidth="xl">
+      <div className="bg-secondary text-primary py-12 px-5 md:py-20">
+        <h2 className="text-xl md:text-2xl font-lora font-semibold text-center">
+          BURJ AL ARIF
+        </h2>
+        <div className="py-4 flex gap-4 justify-center">
+          <a href="#">
+            <FacebookIcon sx={{ fontSize: "40px" }} />
+          </a>
+          <a href="#">
+            <InstagramIcon sx={{ fontSize: "40px" }} />
+          </a>
+          <a href="#">
+            <LinkedInIcon sx={{ fontSize: "40px" }} />
+          </a>
+        </div>
+        <p className="text-center text-[#9da582] rounded-md">
+          <small>
+            Copyright © {new Date().getFullYear()} - All right reserved
+          </small>
+        </p>
+      </div>
+    </Container>
+  );
 };
 
 export default Footer;
