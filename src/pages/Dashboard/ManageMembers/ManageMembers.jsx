42e6691f96b08579178c5c2ee779cf8395831e62
brcshakil1 <brcshakil72@gmail.com>
2023-11-28 01:09:22 +0600

    used mui table

--- src/pages/Dashboard/ManageMembers/ManageMembers.jsx
+++ src/pages/Dashboard/ManageMembers/ManageMembers.jsx
@@ -1,5 +1,74 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hook/useAxiosSecure";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
 const ManageMembers = () => {
-  return <div>manage members</div>;
+  const axiosSecure = useAxiosSecure();
+
+  const { data: members } = useQuery({
+    queryKey: ["members"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get("/users?role=member");
+      return data;
+    },
+  });
+
+  console.log(members);
+
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="left">Name</StyledTableCell>
+              <StyledTableCell align="left">Email</StyledTableCell>
+              <StyledTableCell align="left">Action</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {members?.map((member) => (
+              <StyledTableRow key={member.name}>
+                <StyledTableCell align="left">{member.name}</StyledTableCell>
+                <StyledTableCell align="left">{member.email}</StyledTableCell>
+                <StyledTableCell align="left">
+                  <Button>Remove</Button>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default ManageMembers;
