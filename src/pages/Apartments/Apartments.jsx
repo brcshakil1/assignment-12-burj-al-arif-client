d813247ea550291cbb521989b2e11f0ad3d35441
brcshakil1 <brcshakil72@gmail.com>
2023-12-28 20:49:01 +0600

    added animation on home and apartments page

--- src/pages/Apartments/Apartments.jsx
+++ src/pages/Apartments/Apartments.jsx
@@ -16,11 +16,13 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../hook/useAxiosPublic";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Pagination from "@mui/material/Pagination";
 import useAuth from "./../../hook/useAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Apartments = () => {
   const axiosPublic = useAxiosPublic();
@@ -40,16 +42,6 @@ const Apartments = () => {
       return res.data;
     },
   });
-
-  if (isPending) {
-    return (
-      <div className="min-h-[90vh] grid place-items-center">
-        <Box sx={{ display: "flex" }}>
-          <CircularProgress className="text-secondary" />
-        </Box>
-      </div>
-    );
-  }
 
   const totalPage = Math.ceil(apartments?.totalApartments / limit);
 
@@ -77,117 +69,134 @@ const Apartments = () => {
     }
   };
 
+  useEffect(() => {
+    Aos.init({
+      offset: 200,
+      duration: 600,
+      easing: "ease-out-cubic",
+      delay: 100,
+    });
+  }, []);
+
   //console.log(floor);
 
   return (
     <Container maxWidth="xl">
-      <div>
-        <div
-          className="h-[40vh]  md:h-[60vh]  w-full mt-4 bg-cover md:bg-center bg-no-repeat"
-          style={{ backgroundImage: `url(${apartmentImg})` }}
-        ></div>
-        <div className="py-10">
-          <SectionTitle
-            title="Apartments"
-            justify="justify-center"
-          ></SectionTitle>
+      {isPending ? (
+        <div className="min-h-[90vh] grid place-items-center">
+          <Box sx={{ display: "flex" }}>
+            <CircularProgress className="text-secondary" />
+          </Box>
         </div>
-        <div className="pb-5">
-          <FormControl className="w-28">
-            <InputLabel id="demo-simple-select-label">
-              Filter by floor
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={floor}
-              label="Floor Number"
-              onChange={(e) => setFloor(e.target.value)}
-            >
-              <MenuItem value={1}>1</MenuItem>
-              <MenuItem value={2}>2</MenuItem>
-              <MenuItem value={3}>3</MenuItem>
-              <MenuItem value={4}>4</MenuItem>
-              <MenuItem value={5}>5</MenuItem>
-              <MenuItem value={6}>6</MenuItem>
-              <MenuItem value={7}>7</MenuItem>
-              <MenuItem value={8}>8</MenuItem>
-              <MenuItem value={9}>9</MenuItem>
-              <MenuItem value={10}>10</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-        <div className="grid grid-cols-1 md:md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {apartments?.result?.map((apartment) => (
-            <Card key={apartment?._id}>
-              <CardMedia
-                className="h-[200px] md:h-[250px]"
-                component="img"
-                image={apartment?.imageSrc}
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography
-                  className="text-secondary"
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                >
-                  {apartment?.name}
-                </Typography>
-                <Typography
-                  className="text-secondary"
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  Floor No: {apartment?.floorNo}
-                </Typography>
-                <Typography
-                  className="text-secondary"
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  Block Name: {apartment?.blockName}
-                </Typography>
-                <Typography
-                  className="text-secondary"
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  Apartment No: {apartment?.apartmentNo}
-                </Typography>
-                <Typography
-                  className="text-secondary"
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  Rent Per Month: ${apartment?.rent}
-                </Typography>
+      ) : (
+        <div>
+          <div
+            className="h-[40vh]  md:h-[60vh]  w-full mt-4 bg-cover md:bg-center bg-no-repeat"
+            style={{ backgroundImage: `url(${apartmentImg})` }}
+          ></div>
+          <div className="py-10">
+            <SectionTitle
+              title="Apartments"
+              justify="justify-center"
+            ></SectionTitle>
+          </div>
+          <div className="pb-5">
+            <FormControl className="w-28">
+              <InputLabel id="demo-simple-select-label">
+                Filter by floor
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={floor}
+                label="Floor Number"
+                onChange={(e) => setFloor(e.target.value)}
+              >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={6}>6</MenuItem>
+                <MenuItem value={7}>7</MenuItem>
+                <MenuItem value={8}>8</MenuItem>
+                <MenuItem value={9}>9</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="grid grid-cols-1 md:md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {apartments?.result?.map((apartment) => (
+              <Card key={apartment?._id} data-aos="flip-left">
+                <CardMedia
+                  className="h-[200px] md:h-[250px]"
+                  component="img"
+                  image={apartment?.imageSrc}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography
+                    className="text-secondary"
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                  >
+                    {apartment?.name}
+                  </Typography>
+                  <Typography
+                    className="text-secondary"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    Floor No: {apartment?.floorNo}
+                  </Typography>
+                  <Typography
+                    className="text-secondary"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    Block Name: {apartment?.blockName}
+                  </Typography>
+                  <Typography
+                    className="text-secondary"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    Apartment No: {apartment?.apartmentNo}
+                  </Typography>
+                  <Typography
+                    className="text-secondary"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    Rent Per Month: ${apartment?.rent}
+                  </Typography>
 
-                <Button
-                  onClick={() => handleAgreement(apartment)}
-                  className="bg-tertiary  font-poppins mt-4"
-                  variant="contained"
-                >
-                  Agreement{" "}
-                </Button>
-              </CardContent>
-            </Card>
-          ))}
+                  <Button
+                    onClick={() => handleAgreement(apartment)}
+                    className="bg-tertiary  font-poppins mt-4"
+                    variant="contained"
+                  >
+                    Agreement{" "}
+                  </Button>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+          <div className="flex justify-center py-10">
+            <Pagination
+              onChange={(e) => {
+                setPage(parseInt(e.target.textContent));
+              }}
+              count={totalPage}
+              color="primary"
+              variant="outlined"
+              hidePrevButton
+              hideNextButton
+            />
+          </div>
         </div>
-        <div className="flex justify-center py-10">
-          <Pagination
-            onChange={(e) => {
-              setPage(parseInt(e.target.textContent));
-            }}
-            count={totalPage}
-            color="primary"
-            variant="outlined"
-            hidePrevButton
-            hideNextButton
-          />
-        </div>
-      </div>
+      )}
     </Container>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,36 @@ import AboutBuilding from "./AboutBuilding/AboutBuilding";
 import ContactUs from "./ContactUs/ContactUs";
 import Reviews from "./Reviews/Reviews";
 import Facilities from "./Facilities/Facilities";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    Aos.init({
+      offset: 200,
+      duration: 600,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  }, []);
   return (
     <div>
-      <Banner />
-      <AboutBuilding />
-      <Reviews />
-      <Facilities />
-      <ContactUs />
+      <div data-aos="fade-down">
+        <Banner />
+      </div>
+      <div data-aos="fade-up">
+        <AboutBuilding />
+      </div>
+      <div data-aos="fade-right">
+        <Reviews />
+      </div>
+      <div data-aos="fade-left">
+        <Facilities />
+      </div>
+      <div data-aos="fade-up-right">
+        <ContactUs />
+      </div>
     </div>
   );
 };
